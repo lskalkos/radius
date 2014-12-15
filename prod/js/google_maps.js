8234548844e5bfd9049b2f557d9a421b41122922
Lia Skalkos <lskalkos@boomboxinc.com>
2014-12-15 18:14:57 -0500

    8: Added enter key submission method for homepage (edited google_maps.js), and fixed submit bug with e.preventDefault().

--- prod/js/google_maps.js
+++ prod/js/google_maps.js
@@ -1,12 +1,14 @@
 function initialize() {
 
+
   	var geocoder 	= new google.maps.Geocoder(),
   	input 			= (document.getElementById('pac-input')),
   	autocomplete 	= new google.maps.places.Autocomplete(input);
   	
+  	$('#button').click(function(e) {
+  		e.preventDefault();
 
-  	$('#button').mouseover(function() {
-		var address = $('input').val();
+  		var address = $('input').val();
 
 		if(address) {
 
@@ -14,15 +16,48 @@ function initialize() {
 		    	if (status == google.maps.GeocoderStatus.OK) {
 		    		var longitude 	= results[0].geometry.location.D,
 		    		latitude 		= results[0].geometry.location.k;
+		    		console.log(latitude);
+			   		console.log(longitude);
 		    		$('#latitude').val(latitude);
 		    		$('#longitude').val(longitude);
+		    		$('form').submit();
 		      	} else {
 		        	alert("Geocode was not successful for the following reason: " + status);
 		      	}
 			});
 		}
 
-  	})
+  		
+  	});
+
+
+  	
+  	$('input').keypress(function (e) {
+  		if(e.which == 13)  {
+
+  			e.preventDefault();
+
+	  		var address = $('input').val();
+
+			if(address) {
+
+				geocoder.geocode( {'address': address}, function(results, status) {
+			    	if (status == google.maps.GeocoderStatus.OK) {
+			    		var longitude 	= results[0].geometry.location.D,
+			    		latitude 		= results[0].geometry.location.k;
+			    		console.log(latitude);
+				   		console.log(longitude);
+			    		$('#latitude').val(latitude);
+			    		$('#longitude').val(longitude);
+			    		$('form').submit();
+			      	} else {
+			        	alert("Geocode was not successful for the following reason: " + status);
+			      	}
+				});
+			}		
+  		}
+	});
+
 }
 
 	 	
